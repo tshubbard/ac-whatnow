89c21dd0b39175c1812ad22e9ee751b8c8d0cfef
Travis Hubbard <thubbard@sugarcrm.com>
2020-04-20 14:07:34 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'MFE React Project',
+    clientFileName: 'client.bundle.js',
+    clientCSSFileName: 'client.css',
+    devPort: 9000,
+    projectKey: 'ui_service',
+};
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const projectConfig = require('./config.js');
+
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        port: projectConfig.port,
+        publicPath: this.hmr ? `${projectConfig.host}:${projectConfig.port}/` : '',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src/'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react',
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        browsers: ['last 2 versions', 'ie 11'],
+                                    },
+                                },
+                            ],
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-runtime',
+                            '@babel/plugin-syntax-dynamic-import',
+                            'react-hot-loader/babel',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.(less|css)$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            camelCase: true,
+                            localIdentName: '[local]-[hash:base64:12]',
+                        },
+                    },
+                    'less-loader',
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            title: projectConfig.name,
+            filename: 'index.html',
+            template: '../index.html',
+            projectConfig,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'client.css',
+        }),
+    ],
+};
