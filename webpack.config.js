804a1ab296ad6d2218afc7c25ce526c677fe6358
Travis Hubbard <thubbard@sugarcrm.com>
2020-04-23 11:56:34 -0400

    updated build to only put GA during prod builds

--- webpack.config.js
+++ webpack.config.js
@@ -5,84 +5,87 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const projectConfig = require('./config.js');
 
+module.exports = (env, argv) => {
+    projectConfig.dev = argv.mode === 'development';
 
-module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        port: projectConfig.port,
-        publicPath: this.hmr ? `${projectConfig.host}:${projectConfig.port}/` : '',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                include: path.resolve(__dirname, 'src/'),
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-react',
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                        browsers: ['last 2 versions', 'ie 11'],
+    return {
+        context: path.join(__dirname, 'src'),
+        entry: './index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devServer: {
+            headers: {'Access-Control-Allow-Origin': '*'},
+            port: projectConfig.port,
+            publicPath: this.hmr ? `${projectConfig.host}:${projectConfig.port}/` : '',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    include: path.resolve(__dirname, 'src/'),
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-react',
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        targets: {
+                                            browsers: ['last 2 versions', 'ie 11'],
+                                        },
                                     },
-                                },
+                                ],
                             ],
-                        ],
-                        plugins: [
-                            '@babel/plugin-transform-runtime',
-                            '@babel/plugin-syntax-dynamic-import',
-                            'react-hot-loader/babel',
-                        ],
+                            plugins: [
+                                '@babel/plugin-transform-runtime',
+                                '@babel/plugin-syntax-dynamic-import',
+                                'react-hot-loader/babel',
+                            ],
+                        },
                     },
                 },
-            },
-            {
-                test: /\.(less|css)$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                camelCase: true,
-                                localIdentName: '[local]-[hash:base64:12]',
+                {
+                    test: /\.(less|css)$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    camelCase: true,
+                                    localIdentName: '[local]-[hash:base64:12]',
+                                },
+                                modules: true,
+                                sourceMap: true,
+
                             },
-                            modules: true,
-                            sourceMap: true,
 
                         },
-
-                    },
-                    'less-loader',
-                ],
-            },
-        ]
-    },
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-            title: projectConfig.name,
-            filename: 'index.html',
-            template: '../index.html',
-            projectConfig,
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'client.css',
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: '../data.json',
-                to: 'data.json',
-            },
-        ]),
-    ],
+                        'less-loader',
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new HtmlWebpackPlugin({
+                title: projectConfig.name,
+                filename: 'index.html',
+                template: '../index.html',
+                projectConfig,
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'client.css',
+            }),
+            new CopyWebpackPlugin([
+                {
+                    from: '../data.json',
+                    to: 'data.json',
+                },
+            ]),
+        ],
+    }
 };
